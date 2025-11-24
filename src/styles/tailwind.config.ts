ad941cc063bfc90ae9a0fe82850aab52093b5167
maede97 <maede97@hotmail.com>
2025-11-24 21:07:59 +0100

    improve layout of colors

--- src/styles/tailwind.config.ts
+++ src/styles/tailwind.config.ts
@@ -10,13 +10,17 @@ const config: Config = {
     colors: {
       primary: colors.stone[900],
       secondary: colors.stone[200],
+
       accent: colors.stone[400],
       'accent-dark': colors.stone[600],
-      warning: colors.orange[600],
+
       success: colors.green[600],
+      'success-dark': colors.green[800],
+
+      warning: colors.orange[600],
+
       error: colors.red[600],
       'error-dark': colors.red[800],
-      'success-dark': colors.green[800],
     },
   },
 };
