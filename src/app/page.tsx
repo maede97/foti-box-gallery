737b693760d1c67a58d8720589c1b313f4c968ab
maede97 <maede97@hotmail.com>
2025-11-24 21:09:24 +0100

    change naming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,9 @@ const HomePage: React.FC = () => {
       <h1 className="mb-4 text-4xl font-semibold tracking-wide uppercase">foti-box.com</h1>
 
       <span className="text-sm tracking-wide uppercase">
-        Anfrage?{' '}
-        <a
-          className="text-secondary hover:text-accent underline"
-          href="mailto:anfrage@foti-box.com"
-        >
-          anfrage@foti-box.com
+        Box Mieten?{' '}
+        <a className="text-secondary hover:text-accent underline" href="mailto:mieten@foti-box.com">
+          mieten@foti-box.com
         </a>
       </span>
     </div>
