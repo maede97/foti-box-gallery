25e8052c65b1dace7a48fde1d66627e501ac2fdc
maede97 <maede97@hotmail.com>
2025-12-13 13:44:32 +0100

    update image type

--- src/models/image.ts
+++ src/models/image.ts
@@ -1,8 +1,8 @@
-import mongoose, { Document, Schema, Types } from 'mongoose';
+import mongoose, { Document, Schema } from 'mongoose';
 
 export interface IImage extends Document {
   uuid: string;
-  event: Types.ObjectId; // Reference to Event
+  event: Schema.Types.ObjectId; // Reference to Event
   extension: string;
   createdAt: Date;
 }
